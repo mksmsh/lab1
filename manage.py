69d4cff7281a6f03fb40c48c155e6c12787eafdf
mksmsh <maksimka.mishura@mail.ru>
2025-01-14 08:00:05 +0300

    19

--- manage.py
+++ manage.py
@@ -1,13 +1,8 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.core.management import execute_from_command_line
 
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lab1.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lab1.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,9 +12,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lab1.settings")
-    port = os.environ.get("PORT", "8000")
-    execute_from_command_line(["manage.py", "runserver", f"0.0.0.0:{port}"])
